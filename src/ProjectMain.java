6acc22d17d37397e8e517fcb3ba886ca8e89bed7
hamburger <>
2023-11-20 19:36:49 -0500

    Add marks to students

--- src/ProjectMain.java
+++ src/ProjectMain.java
@@ -1,42 +1,63 @@
 package src;
 
-import java.util.*;
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
 
 public class ProjectMain {
 
-    // Constants
-    private static final String LINE = "-".repeat(40);
-    private static final String TEST_LINE = "=".repeat(80);
+	// Constants
+	private static final String LINE = "-".repeat(40);
+	private static final String TEST_LINE = "=".repeat(80);
+	final static String COURSE_FILE_PATH = "./input/CourseFile.txt";
+	final static String NAME_FILE_PATH = "./input/NameFile.txt";
+	final static String OUTPUT_FILE_NAME = "./output/Output.txt";
 
-    private Set<Student> setOfStudents = new HashSet<Student>();
+	private static Set<Student> setOfStudents = new HashSet<Student>();
 
-    /**
-     * For testing. Reads contents of "CourseFile.txt" and "NameFile.txt" from root of project
-     * and outputs to "OutputFile.txt".
-     *
-     * @param args Unused.
-     */
-    public static void main(String[] args) {
-        System.out.println("CP317 Project Class Tests");
+	/**
+	 * For testing. Reads contents of "CourseFile.txt" and "NameFile.txt" from root
+	 * of project and outputs to "OutputFile.txt".
+	 *
+	 * @param args Unused.
+	 */
+	public static void main(String[] args) {
+		System.out.println("CP317 Project Class Tests");
 
-        String coursePath = "input/CourseFile.txt";
-        File courseFile = new File(coursePath);
-        String namePath = "input/NameFile.txt";
-        File nameFile = new File(namePath);
-        testStudent();
-    }
+		String coursePath = "input/CourseFile.txt";
+		File courseFile = new File(coursePath);
+		String namePath = "input/NameFile.txt";
+		File nameFile = new File(namePath);
+		testStudent();
+		testReadFile();
+	}
 
-    /**
-     * Simple src.Student tests.
-     */
-    public static void testStudent() {
-        System.out.println(TEST_LINE);
-        System.out.println("Testing src.Student");
-        System.out.println(LINE);
-        Student student1 = new Student("John Smith", 200713471);
-        System.out.println(student1.toString());
-    }
+	/**
+	 * Simple src.Student tests.
+	 */
+	public static void testStudent() {
+		System.out.println(TEST_LINE);
+		System.out.println("Testing src.Student");
+		System.out.println(LINE);
+		Student student1 = new Student("John Smith", 200713471);
+		System.out.println(student1.toString());
+	}
+
+	/**
+	 * Simple src.ReadFile tests.
+	 */
+	public static void testReadFile() {
+		System.out.println("Testing src.ReadFile");
+		System.out.println(LINE);
+		ReadFile readFile = new ReadFile();
+		File courseFile = new File(COURSE_FILE_PATH);
+		File nameFile = new File(NAME_FILE_PATH);
+		ReadFile.readFile(courseFile, nameFile, setOfStudents);
+		Iterator<Student> studentsIterator = setOfStudents.iterator();
+		while (studentsIterator.hasNext()) {
+			Student currentStudent = studentsIterator.next();
+			currentStudent.getMarks();
+		}
+	}
 }
--- src/Student.java
+++ src/Student.java
@@ -2,7 +2,7 @@ package src;
 
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.HashSet;
+import java.util.Map;
 
 /**
  * Inherited class.
@@ -10,44 +10,53 @@ import java.util.HashSet;
  * @author Team
  * @version 2023-11-06
  */
-public class Student{
+public class Student {
 
-    protected String name;
-    protected int studentId;
-    protected HashMap<String, ArrayList<Integer>> studentAllMarks = new HashMap<String, ArrayList<Integer>>();
-    public HashMap<String, Integer> studentFinalMarks = new HashMap<String, Integer>();
+	protected String name;
+	protected int studentId;
+	protected HashMap<String, ArrayList<Integer>> studentAllMarks = new HashMap<String, ArrayList<Integer>>();
+	public HashMap<String, Integer> studentFinalMarks = new HashMap<String, Integer>();
 
-    /**
-     * src.Student constructor
-     *
-     *
-     * @param name src.Student first name (given name): defined in src.Person
-     * @param studentId        src.Student id number
-     */
-    public Student(final String name, final int studentId) {
-	    this.name = name;
-	    this.studentId = studentId;
-    }
+	/**
+	 * src.Student constructor
+	 *
+	 *
+	 * @param name      src.Student name (given name): defined in src.Person
+	 * @param studentId src.Student id number
+	 */
+	public Student(final String name, final int studentId) {
+		this.name = name;
+		this.studentId = studentId;
+	}
 
-    /**
-     * Getter for studentId.
-     *
-     * @return this.studentId
-     */
-    public int getId() {
-	    return this.studentId;
-    }
+	/**
+	 * Getter for studentId.
+	 *
+	 * @return this.studentId
+	 */
+	public int getId() {
+		return this.studentId;
+	}
 
-    /**
-     * Creates formatted String version of src.Student.
-     */
-    @Override
-    public String toString() {
-	    return (this.name + ": " + this.studentId);
-    }
+	/**
+	 * Creates formatted String version of src.Student.
+	 */
+	@Override
+	public String toString() {
+		return (this.name + ": " + this.studentId);
+	}
 
-    public void setMarks(String courseName, ArrayList<Integer> marks){
-        this.studentAllMarks.put(courseName, marks);
-    }
+	public void setMarks(String courseName, ArrayList<Integer> marks) {
+		this.studentAllMarks.put(courseName, marks);
+	}
+
+	public void getMarks() {
+		for (Map.Entry<String, ArrayList<Integer>> student : studentAllMarks.entrySet()) {
+			String key = student.getKey();
+			ArrayList<Integer> value = student.getValue();
+			System.out.println(
+					"Name: " + this.name + ", ID: " + this.studentId + ", Course: " + key + ", Marks: " + value);
+		}
+	}
 
 }
